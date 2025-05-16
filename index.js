8ad4ccb8457b226b5fcd3cf1f02a660e3faca027
aamidou <akimayena26@gmail.com>
2025-05-16 15:17:50 +0100

    add test route

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/test', (req, res) => {
+  return res.status(200).json({
+    message: 'Hello from the backend API!',
+  })
+})
+
 app.listen(PORT, () => {
   console.log(
     `Server is running on port ${PORT} in ${ENV} mode`.yellow.underline.bold
