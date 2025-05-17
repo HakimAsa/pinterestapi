3bf63f8612daa50397abde031ab96a9d46f7025c
aamidou <akimayena26@gmail.com>
2025-05-17 09:35:26 +0100

    add comment schema

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,18 +1,43 @@
 import mongoose from 'mongoose'
 import Joi from 'joi'
+import JoiObjectId from 'joi-objectid'
+
 import models from '../utils/models'
 
-const commentSchema = new mongoose.Schema({
-  // Define your schema here
-})
+const Schema = mongoose.Schema
+const myJoiObjectId = JoiObjectId(Joi)
+
+const commentSchema = new Schema(
+  {
+    // Define your schema here
+    description: {
+      type: String,
+      required: true,
+    },
+    pin: {
+      type: Schema.Types.ObjectId,
+      ref: models.PIN,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: models.USER,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
 
 const Comment = mongoose.model(models.COMMENT, commentSchema)
 
 export const validateComment = (data, isRequired = true) => {
   const schema = Joi.object({
     // Define your validation schema here
+    description: isRequired ? Joi.string().required() : Joi.string(),
+    pin: isRequired ? myJoiObjectId().required() : myJoiObjectId(),
+    user: myJoiObjectId().required(),
   })
   return schema.validate(data)
 }
 
-export default comment
+export default Comment
